5d3640c8631b82010320c8bfd6124860ddd921a1
olgierd <opilarczyk@gmail.com>
2020-06-05 23:05:16 +0200

    Add calibration mode & saving cal to EEPROM

--- powermeter.ino
+++ powermeter.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <EEPROM.h>
 
 #define DEFAULT_MODE 0
 #define DEFAULT_AVERAGING 6
@@ -18,6 +19,7 @@
 
 #define BTN_SET 10
 #define BTN_SELECT 9
+#define BTN_GROUND 8
 
 #define CAL_LOW -40
 #define CAL_UP -10
@@ -40,13 +42,17 @@ double cal_b[] =      { 0,     0,     0,     0,     0,     0,     0,     0,    0
 uint16_t avg_values[] = { 1, 5, 10, 50, 100, 500, 1000, 2000, 5000, 10000};
 
 #define SELECTIONS_AVAIL 6
-enum btn_select{SEL_UNITS, SEL_BAND, SEL_MODE, SEL_AVG, SEL_ATT, SEL_NONE};
+enum btn_select{SEL_UNITS, SEL_BAND, SEL_MODE, SEL_AVG, SEL_ATT, SEL_NONE, SEL_SAVE, SEL_CAL_LEVEL};
+
+#define SELECTIONS_CAL 5
+int calibration_selections[] = { SEL_BAND, SEL_MODE, SEL_AVG, SEL_CAL_LEVEL, SEL_SAVE };
 
 #define UNITS_AVAIL 2
-enum measure_units{DBM, MW};
+enum measure_units{DBM, RAW};
+char *units_labels[] = { "dBm", "RAW"};
 
-int mode=DEFAULT_MODE, averaging=DEFAULT_AVERAGING, band=DEFAULT_BAND, current_sel=SEL_NONE, units;
-int lowest, highest, r, att, display_value;
+int mode=DEFAULT_MODE, averaging=DEFAULT_AVERAGING, band=DEFAULT_BAND, current_sel=SEL_NONE, units=SEL_UNITS;
+int lowest, highest, r, att, display_value, calibration_mode=0, current_sel_cal=SEL_BAND, cal_level=0;
 uint32_t cumulative;
 char k;
 
@@ -57,7 +63,7 @@ void setup() {
   pinMode(2, OUTPUT);
   digitalWrite(2, HIGH);
 
-  delay(200);
+  delay(100);
 
   analogReference(EXTERNAL);
   Serial.begin(115200);
@@ -67,20 +73,32 @@ void setup() {
   pinMode(BTN_SELECT, INPUT_PULLUP);
 
   // ground for buttons
-  pinMode(12, OUTPUT);
-  pinMode(7, OUTPUT);
-  digitalWrite(12, LOW);
-  digitalWrite(7, LOW);
+  pinMode(BTN_GROUND, OUTPUT);
+  digitalWrite(BTN_GROUND, LOW);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
 
   prepare_calib();
+
+  if(!digitalRead(BTN_SET) || !digitalRead(BTN_SELECT)) {
+    calibration_mode = 1;
+    units = RAW;
+    
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(4);
+    display.print("CALIB :)");
+    display.display();
+    delay(3000);
+  }
 }
 
-// prepare "a" & "b" coefficients for calibration
-void prepare_calib() {
+// read values from EEPROM and prepare "a" & "b" coefficients for calibration
+void prepare_calib() {  
   for(int i=0; i<BANDS; i++) {
+    cal_lower[i] = (EEPROM.read(i*4) << 8) + EEPROM.read(i*4+1);
+    cal_upper[i] = (EEPROM.read(i*4+2) << 8) + EEPROM.read(i*4+3);
     cal_a[i] = (float)(CAL_LOW-CAL_UP)/(cal_lower[i]-cal_upper[i]);
     cal_b[i] = -((double)cal_lower[i]*cal_a[i]-CAL_LOW);
   }
@@ -108,13 +126,19 @@ void display_all(int val) {
   display.setTextSize(3);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.print(get_dbm(val), 1);
+  switch(units) {
+    case DBM:
+      display.print(get_dbm(val), 1);
+      break;
+    case RAW:
+      display.print(val);
+  }
 
   // print units
   display.setTextSize(2);
   display.setCursor(90, 0);
   invert_text_if_selected(SEL_UNITS);
-  display.print("dBm");
+  display.print(units_labels[units]);
   reset_text_color();
 
   // print band label
@@ -143,14 +167,36 @@ void display_all(int val) {
   }
   reset_text_color();
 
-  // print attenuator value
-  display.setCursor(48, 24);
-  display.print("ATT: ");
-  invert_text_if_selected(SEL_ATT);
-  display.print(att);
-  reset_text_color();
+  if(!calibration_mode) {
+    // print attenuator value
+    display.setCursor(48, 24);
+    display.print("ATT: ");
+    invert_text_if_selected(SEL_ATT);
+    display.print(att);
+    reset_text_color();
+  }
+
+  if(calibration_mode) {
+    // change calibration level
+    display.setCursor(48, 24);
+    invert_text_if_selected(SEL_CAL_LEVEL);
+    display.print("LV: ");
+    display.print(cal_level == 0 ? CAL_LOW : CAL_UP);
+    reset_text_color();
+  
+    // save calibration
+    display.setCursor(104, 24);
+    invert_text_if_selected(SEL_SAVE);
+    display.print("SAVE");
+    reset_text_color();
+  }
 
   display.display();
+}
+
+void save_calibration() {
+  EEPROM.write(band*4 + cal_level*2, (display_value>>8));
+  EEPROM.write(band*4 + cal_level*2 + 1, (display_value&0xff));
 }
 
 void set_action() {
@@ -158,10 +204,36 @@ void set_action() {
   if(current_sel == SEL_BAND) band = (band+1)%BANDS;
   if(current_sel == SEL_AVG) averaging = (averaging+1)%AVERAGE_MODES;
   if(current_sel == SEL_ATT) att = (att + 10)%MAX_ATT;
+  if(current_sel == SEL_UNITS) units = (units+1)%UNITS_AVAIL;
+  if(current_sel == SEL_CAL_LEVEL) cal_level = (cal_level+1)%2;
+  if(current_sel == SEL_SAVE) save_calibration();
 }
 
 void select_action() {
-  current_sel = (current_sel + 1) % SELECTIONS_AVAIL;
+  if(calibration_mode) {
+    current_sel_cal = (current_sel_cal+1)%SELECTIONS_CAL;
+    current_sel = calibration_selections[current_sel_cal];
+  }
+  else {
+    current_sel = (current_sel + 1) % SELECTIONS_AVAIL;    
+  }
+}
+
+void print_calibration() {
+  Serial.println("Calibration...");
+  Serial.print("Lower point: ");
+  Serial.println(CAL_LOW);
+  Serial.print("Upper point: ");
+  Serial.println(CAL_UP);
+  
+  for(uint8_t i = 0; i<BANDS; i++) {
+    Serial.print(band_labels[i]);
+    Serial.print(" L: ");
+    Serial.print(cal_lower[i]);
+    Serial.print(" H: ");
+    Serial.println(cal_upper[i]);
+  }
+  
 }
 
 void serial_parse() {
@@ -170,6 +242,7 @@ void serial_parse() {
   if(k=='b') band = (band+1)%BANDS;
   if(k=='a') averaging = (averaging+1)%AVERAGE_MODES;
   if(k=='t') att = (att + 10)%MAX_ATT;
+  if(k=='p') print_calibration();
 
   // simulation of physical buttons
   if(k==',') {
